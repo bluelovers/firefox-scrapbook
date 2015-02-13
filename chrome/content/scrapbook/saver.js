d46a63d350b45e61fbd372504951cacd10331efc
bluelovers <codelovers@users.sourceforge.net>
2015-02-13 11:08:23 +0800

    fileLR = this.handleFileName(fileLR, newFileName, aURLSpec, aDocumentSpec, sNodeData);

--- chrome/content/scrapbook/saver.js
+++ chrome/content/scrapbook/saver.js
@@ -1112,7 +1112,7 @@ var sbContentSaver = {
 	/**
 	 * @return  [(string) newFileName, (bool) isDuplicated]
 	 */
-	getUniqueFileName: function(newFileName, aURLSpec, aDocumentSpec)
+	getUniqueFileName: function(newFileName, aURLSpec, aDocumentSpec, sNodeData)
 	{
 		if ( !newFileName ) newFileName = "untitled";
 		newFileName = newFileName;
@@ -1122,6 +1122,9 @@ var sbContentSaver = {
 		if ( !fileLR[1] ) fileLR[1] = "dat";
 		aURLSpec = sbCommonUtils.splitURLByAnchor(aURLSpec)[0];
 		var seq = 0;
+
+		fileLR = this.handleFileName(fileLR, newFileName, aURLSpec, aDocumentSpec, sNodeData);
+
 		newFileName = fileLR[0] + "." + fileLR[1];
 		var newFileNameCI = newFileName.toLowerCase();
 		while ( this.file2URL[newFileNameCI] != undefined ) {
